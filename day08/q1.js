f716db93f12cd3dedea2d36856abfb35ecec120d
LeeDaeGyeong <dleorud1027@gmail.com>
2024-08-15 23:59:11 +0900

    programmers day08

--- day08/q1.js
+++ day08/q1.js
@@ -2,7 +2,9 @@
     음양 더하기
 
     문제 설명
-    어떤 정수들이 있습니다. 이 정수들의 절댓값을 차례대로 담은 정수 배열 absolutes와 이 정수들의 부호를 차례대로 담은 불리언 배열 signs가 매개변수로 주어집니다. 실제 정수들의 합을 구하여 return 하도록 solution 함수를 완성해주세요.
+    어떤 정수들이 있습니다. 
+    이 정수들의 절댓값을 차례대로 담은 정수 배열 absolutes와 이 정수들의 부호를 차례대로 담은 불리언 배열 signs가 매개변수로 주어집니다. 
+    실제 정수들의 합을 구하여 return 하도록 solution 함수를 완성해주세요.
 
     제한사항
     absolutes의 길이는 1 이상 1,000 이하입니다.
@@ -17,6 +19,20 @@
 */
 
 const solution = (absolutes, signs) => {
-    const answer = 123456789;
-    return answer;
+
+    const answerArr = []
+
+    for(let i = 0 ; i < signs.length ; i++){
+        if(signs[i] === false){
+            answerArr.push(-absolutes[i])
+        } else {
+            answerArr.push(absolutes[i])
+        }
+    }
+
+    const answer = answerArr.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+    }, 0)
+
+    return answer
 }
